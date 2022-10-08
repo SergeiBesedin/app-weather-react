992b0a6d2297052b030dbff49c553461bfe922cb
SergeiBesedin <sergey-besedin98@mail.ru>
2022-10-08 17:43:49 +0300

    доработки для header

--- src/components/header-actions/header-actions.tsx
+++ src/components/header-actions/header-actions.tsx
@@ -1,4 +1,5 @@
 import { Button } from '../ui/button/button'
+import { ReactComponent as Search } from '../../assets/icons/search.svg'
 import { ReactComponent as Settings } from '../../assets/icons/settings.svg'
 import styles from './header-actions.module.scss'
 
@@ -9,6 +10,9 @@ export function HeaderActions() {
 
     return (
         <div className={styles.actions}>
+            <Button classes={[styles.search]} disabled={false} onClick={click}>
+                <Search />
+            </Button>
             <Button classes={[styles.settings]} disabled={false} onClick={click}>
                 <Settings />
             </Button>
