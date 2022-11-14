5e9d2f692b072bd0e9e80a01f370a19d1915e41e
SergeiBesedin <sergey-besedin98@mail.ru>
2022-11-14 22:26:44 +0300

    логика для слайдера и адаптив

--- src/components/content/hourly-forecast/hourly-forecast.tsx
+++ src/components/content/hourly-forecast/hourly-forecast.tsx
@@ -1,55 +1,52 @@
 import { useRef } from 'react'
 import { IWeather } from './../../../typings/typings'
-import { dateFormat } from '../../../utils/utils'
 import { Tile } from '../tile/tile'
 import { Button } from '../../ui/button/button'
 import { HourlyForecastItem } from './hourly-forecast-item/hourly-forecast-item'
 import styles from './hourly-forecast.module.scss'
 
-const TIMESTAMPS = 8 // Количество временных меток за один день, т.к. 24 / 3 = 8
+const UNIX_TIME_DAY = 86400 // день, переведенный в unix
 interface HourlyForecastProps {
     items: Array<IWeather>
     tempUnit: string
 }
 
 export function HourlyForecast({ items, tempUnit }: HourlyForecastProps) {
-    const listItemsRect = useRef(null)
+    const wrapperRef = useRef<HTMLDivElement>(null)
+    const listItemsRef = useRef<HTMLUListElement>(null)
+
+    const filteredItems = items.filter((item) => item.dt <= items[0].dt + UNIX_TIME_DAY) // показываем в карусели только 9 временных меток
 
     const title = 'Почасовой прогноз'
 
-    const currentDayTimestamps =
-        TIMESTAMPS + 1 - Number(dateFormat(items[0].dt * 1000, { hour: 'numeric' })) / 3 // считаем сколько меток осталось в текущем дне
-    // Показываем оставшиеся метки за текущий день + все метки следующего дня
-    const filteredItems = items.slice(0, currentDayTimestamps + TIMESTAMPS)
-
     const arrowClickHandler = (toLeft: boolean) => {
-        // let shift = (toLeft ? -1 : 1) * (nodesRect.width - this.nodeWidth)
-        // this.view.nodes.scrollBy({
-        //     left: shift,
-        //     behavior: 'smooth',
-        // })
+        const containerWidth = wrapperRef.current?.getBoundingClientRect().width
+        if (!containerWidth) return
+        const shift = (toLeft ? -1 : 1) * (containerWidth - 50)
+        listItemsRef.current?.scrollBy({
+            left: shift,
+            behavior: 'smooth',
+        })
     }
 
     return (
         <Tile title={title} classes={[styles.tileContainer]}>
-            <div className={styles.wrapper} ref={listItemsRect}>
+            <div className={styles.wrapper} ref={wrapperRef}>
                 <Button
                     classes={[styles.leftArrow]}
                     disabled={false}
                     onClick={() => arrowClickHandler(true)}
                 />
-                <ul className={styles.listItems}>
-                    {filteredItems.map((item) => {
-                        return (
-                            <HourlyForecastItem
-                                key={item.dt}
-                                timestamp={item.dt}
-                                weather={item.weather[0].main}
-                                temp={item.main.temp}
-                                tempUnit={tempUnit}
-                            />
-                        )
-                    })}
+                <ul className={styles.listItems} ref={listItemsRef}>
+                    {filteredItems.map((item) => (
+                        <HourlyForecastItem
+                            key={item.dt}
+                            timestamp={item.dt}
+                            weather={item.weather[0].main}
+                            temp={item.main.temp}
+                            tempUnit={tempUnit}
+                        />
+                    ))}
                 </ul>
                 <Button
                     classes={[styles.rightArrow]}
