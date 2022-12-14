bb8538cf6b416a17f07d59e144b21d8dc86b4e71
SergeiBesedin <sergey-besedin98@mail.ru>
2022-12-14 22:40:05 +0300

    вынес всю логику в отдельные функции, добавил иконки, поправил стили

--- src/components/content/weather-tomorrow/weather-tomorrow.tsx
+++ src/components/content/weather-tomorrow/weather-tomorrow.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { IWeather } from '../../../typings/typings'
+import { generateMessageText } from '../../../utils/generate-message-text'
+import { getIcon } from '../../../utils/get-icon'
+import { Image } from '../../ui/image/image'
 import styles from './weather-tomorrow.module.scss'
 
 interface WeatherTomorrowProps {
@@ -9,31 +12,17 @@ interface WeatherTomorrowProps {
 }
 
 function WeatherTomorrow({ curTemp, tempUnit, list }: WeatherTomorrowProps) {
-    const listItem = list.find((item) => item.dt_txt?.includes('12:00:00')) // находим нужную временную метку
-    const tempCurrent =
-        tempUnit === 'fahrenheit' ? Math.round(curTemp * (9 / 5) + 32) : Math.round(curTemp)
-    const tempTomorrow =
-        tempUnit === 'fahrenheit'
-            ? Math.round(listItem!.main.temp * (9 / 5) + 32)
-            : Math.round(listItem!.main.temp)
-
-    const difference = Math.abs(tempCurrent - tempTomorrow) // считаем разницу в температуре
-
-    // TODO рефактор
-    const message = []
-
-    if (tempCurrent > difference) {
-        message.push(`Завтра ${tempTomorrow}°, на ${difference}° холоднее, чем сегодня`)
-    } else if (tempCurrent === difference) {
-        message.push('Завтра будет такая же температура, как и сегодня')
-    } else {
-        message.push(`Завтра ${tempTomorrow}°, на ${difference}° теплее, чем сегодня`)
-    }
+    const message = generateMessageText(curTemp, tempUnit, list)
 
     return (
         <div className={styles.weatherTomorrow}>
             <div className={styles.container}>
-                <p>{message.join(';')}</p>
+                <Image
+                    alt={message.type}
+                    src={getIcon(message.type + 'tomorrow')}
+                    classes={[styles.picture]}
+                />
+                <p className={styles.message}>{message.message}</p>
             </div>
         </div>
     )
